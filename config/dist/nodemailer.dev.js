6d469e3ee593f1f122050ba92ab2f1135659f9c1
Monishka0709 <monishka0709@gmail.com>
2026-05-19 09:41:37 +0530

    fix8

--- config/dist/nodemailer.dev.js
+++ config/dist/nodemailer.dev.js
@@ -11,7 +11,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 var transporter = _nodemailer["default"].createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false, 
     auth: {
         user: process.env.SMTP_USER,
